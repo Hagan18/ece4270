6cd9fd19bf780a6ddabc593026c7b84a84cd2386
Kyle Hagan <hagan18@gmail.com>
2017-09-17 23:23:04 +0000

    slowly but surely

--- lab2/MIPS_assembler.c
+++ lab2/MIPS_assembler.c
@@ -35,15 +35,21 @@ int main(int argc, char *argv[]) {
             rd = getRegister(reg1);
             rs = getRegister(reg2);
             rt = getRegister(offset);
-            hexInstruction = (rs << 21) | (rt << 16) | (rd << 11) | 0x20;
+            hexInstruction = r_type_format(0x20,rd,rs,rt);
             printf("ADD: %x\n",hexInstruction);
             
 		}
 		else if (strcmp("addu", instruction)== 0){//
-		    r_type_format(0x21,reg1,reg2,offset);
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            rt = getRegister(offset);
+		    r_type_format(0x21,rd,rs,rt);
 		}
 		else if (strcmp("addi", instruction)== 0){//
-		    i_type_format(0x08,reg1,reg2,offset);
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            immediate = getRegister(offset);
+		    i_type_format(0x08,rd,rt,immediate);
 		}
 		else if (strcmp("addiu", instruction)== 0){//
 		  //  i_type_format(0x09,reg1,reg2,offset);
@@ -54,85 +60,188 @@ int main(int argc, char *argv[]) {
             printf("ADDIU: %x\nrs: %x, rt: %x, immediate: %x\n",hexInstruction,rs,rt,immediate);
 		}
 		else if (strcmp("sub", instruction)== 0){
-		    
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            rt = getRegister(offset);
+		    hexInstruction = r_type_format(0x22,rd,rs,rt);
 		}
 		else if (strcmp("subu", instruction)== 0){
+		    //0x23
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            rt = getRegister(offset);
+		    hexInstruction = r_type_format(0x23,rd,rs,rt);
 		}
 		else if (strcmp("mult", instruction)== 0){
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+		    hexInstruction = r_type_format(0x18,rd,rs,0x0);
 		}
 		else if (strcmp("multu", instruction)== 0){
+		    //since there are only 2 registers used, pass rt as 0x0
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+		    hexInstruction = r_type_format(0x19,rd,rs,0x0);
 		}
 		else if (strcmp("div", instruction)== 0){
+		  //since there are only 2 registers used, pass rt as 0x0
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+		    hexInstruction = r_type_format(0x1A,rd,rs,0x0);
 		}
 		else if (strcmp("divu", instruction)== 0){
+		    //since there are only 2 registers used, pass rt as 0x0
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+		    hexInstruction = r_type_format(0x1B,rd,rs,0x0);
 		}
 		else if (strcmp("and", instruction)== 0){//
+		  //  0x24
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            rt = getRegister(offset);
+		    hexInstruction = r_type_format(0x24,rd,rs,rt);
 		}
-		else if (strcmp("andi", instruction)== 0){//
+		else if (strcmp("andi", instruction)== 0){
+		  //  0xC
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            immediate = getRegister(offset);
+		    hexInstruction = i_type_format(0xC,rs,rt,immediate);
 		}
 		else if (strcmp("or", instruction)== 0){
+		    //0x25
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            rt = getRegister(offset);
+		    hexInstruction = r_type_format(0x25,rd,rs,rt);
 		}
 		else if (strcmp("ori", instruction)== 0){
+		  //  0xD
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            immediate = getRegister(offset);
+		    hexInstruction = i_type_format(0xD,rs,rt,immediate);
 		}
 		else if (strcmp("xor", instruction)== 0){
+		  //  0x26
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            rt = getRegister(offset);
+		    hexInstruction = r_type_format(0x26,rd,rs,rt);
 		}
 		else if (strcmp("xori", instruction)== 0){
+		  //  0xE
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            immediate = getRegister(offset);
+		    hexInstruction = i_type_format(0xE,rs,rt,immediate);
 		}
 		else if (strcmp("nor", instruction)== 0){
+		  //  0x27
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            rt = getRegister(offset);
+		    hexInstruction = r_type_format(0x27,rd,rs,rt);
 		}
 		else if (strcmp("slt", instruction)== 0){
+		  //  0x2A
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            rt = getRegister(offset);
+		    hexInstruction = r_type_format(0x2A,rd,rs,rt);
 		}
 		else if (strcmp("slti", instruction)== 0){
+		  //  0xA
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            immediate = getRegister(offset);
+		    hexInstruction = i_type_format(0xA,rs,rt,immediate);
 		}
 		else if (strcmp("sll", instruction)== 0){
+		  //  0x0
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            rt = getRegister(offset);
+		    hexInstruction = r_type_format(0x0,rd,rs,rt);
 		}
 		else if (strcmp("srl", instruction)== 0){
+		  //  0x02
+		    //special case
 		}
 		else if (strcmp("sra", instruction)== 0){
+		  //  0x3
+		  //special case
 		}
 		else if (strcmp("lw", instruction)== 0){
+		  //  0x23
+		    rd = getRegister(reg1);
+            rs = getRegister(reg2);
+            immediate = getRegister(offset);
+		    hexInstruction = i_type_format(0x23,rs,rt,immediate);
 		}
 		else if (strcmp("lb", instruction)== 0){
+		  //  0x20
 		}
 		else if (strcmp("lh", instruction)== 0){
+		  //  0x21
 		}
 		else if (strcmp("lui", instruction)== 0){
+		  //  0xF
 		}
 		else if (strcmp("sw", instruction)== 0){
+		  //  0x2B
 		}
 		else if (strcmp("sb", instruction)== 0){
+		  //  0x28
 		}
 		else if (strcmp("sh", instruction)== 0){
+		  //  0x29
 		}
 		else if (strcmp("mfhi", instruction)== 0){
+		  //  0x10
 		}
 		else if (strcmp("mflo", instruction)== 0){
+		  //  0x12
 		}
 		else if (strcmp("mthi", instruction)== 0){
+		  //  0x11
 		}
 		else if (strcmp("mtlo", instruction)== 0){
+		  //  0x13
 		}
 		else if (strcmp("beq", instruction)== 0){
+		  //  0x04
 		}
 		else if (strcmp("bne", instruction)== 0){
+		  //  0x5
 		}
 		else if (strcmp("blez", instruction)== 0){
+		  //  0x6
 		}
 		else if (strcmp("bltz", instruction)== 0){
+		  //  0x01
 		}
 		else if (strcmp("bgez", instruction)== 0){
+		  //  0x01
 		}
 		else if (strcmp("bgtz", instruction)== 0){
+		  //  0x07
 		}
 		else if (strcmp("j", instruction)== 0){
+		  //  0x02
 		}
 		else if (strcmp("jr", instruction)== 0){
+		  //  0x08
 		}
 		else if (strcmp("jal", instruction)== 0){
+		  //  0x03
 		}
 		else if (strcmp("jalr", instruction)== 0){
+		  //  0x05
 		}
 		else if (strcmp("syscall", instruction)== 0){
+		  //  0x0C
 		}
 		
 		else{
